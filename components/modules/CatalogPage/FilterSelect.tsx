12fb489d29d64aa749f592334a0ec5a0881b594c
ssivenkov <ser.sivenkov@gmail.com>
2024-05-03 16:02:21 +0300

    add sorting for "Catalog" page

--- components/modules/CatalogPage/FilterSelect.tsx
+++ components/modules/CatalogPage/FilterSelect.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {ISelectOption, TSelectOption} from "@/types/common";
 import Select from "react-select";
 import {optionStyles} from "@/styles/common/searchInput";
@@ -13,17 +13,33 @@ import {
   selectCategoryOption3
 } from "@/utils/selectContents";
 import {
-  setBoilerParts,
+  $boilerParts,
   setBoilerPartsByPopularity,
   setBoilerPartsChipFirst,
   setBoilerPartsExpensiveFirst
 } from "@/context/boilerParts";
+import {useRouter} from "next/router";
+import {FilterQuery} from "@/constants";
 
 const FilterSelect = () => {
   const mode = useStore($mode);
+  const boilerParts = useStore($boilerParts);
   const [categoryOption, setCategoryOption] = useState<TSelectOption>(null);
-
+  const router = useRouter();
   const defaultSelectString = selectCategoryOption1;
+
+  const updateRouteParam = (first: string) => {
+    router.push(
+      {
+        query: {
+          ...router.query,
+          first
+        }
+      },
+      undefined,
+      { shallow: true }
+    );
+  }
 
   const handleSortOptionChange = (selectedOption: TSelectOption) => {
     setCategoryOption(selectedOption);
@@ -31,20 +47,53 @@ const FilterSelect = () => {
     switch ((selectedOption as ISelectOption).value) {
       case selectCategoryOption1:
         setBoilerPartsChipFirst();
+        updateRouteParam(FilterQuery.CHEAP);
         break;
 
       case selectCategoryOption2:
         setBoilerPartsExpensiveFirst();
+        updateRouteParam(FilterQuery.EXPENSIVE);
         break;
 
       case selectCategoryOption3:
         setBoilerPartsByPopularity();
+        updateRouteParam(FilterQuery.POPULAR);
         break;
 
       default:
         break;
     }
   }
+
+  const updateCategoryOption = (value: string) => {
+    setCategoryOption({ value, label: value })
+  }
+
+  useEffect(() => {
+    if (boilerParts.rows) {
+      switch (router.query.first) {
+        case FilterQuery.CHEAP:
+          updateCategoryOption(selectCategoryOption1);
+          setBoilerPartsChipFirst();
+          break;
+
+        case FilterQuery.EXPENSIVE:
+          updateCategoryOption(selectCategoryOption2);
+          setBoilerPartsExpensiveFirst();
+          break;
+
+        case FilterQuery.POPULAR:
+          updateCategoryOption(selectCategoryOption3);
+          setBoilerPartsByPopularity();
+          break;
+
+        default:
+          updateCategoryOption(selectCategoryOption1);
+          setBoilerPartsChipFirst();
+          break;
+      }
+    }
+  }, [boilerParts.rows, router.query.first]);
 
   return (
     <Select
--- constants/index.ts
+++ constants/index.ts
@@ -2,3 +2,9 @@ export enum HTTPStatus {
   UNAUTHORIZED = 401,
   FORBIDDEN = 403,
 }
+
+export enum FilterQuery {
+  CHEAP = 'cheap',
+  EXPENSIVE = 'expensive',
+  POPULAR = 'popular',
+}
